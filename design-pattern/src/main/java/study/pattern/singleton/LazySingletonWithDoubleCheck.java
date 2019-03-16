1633aeec3bf5295bff46d708ba37ce65a32f06f4
蒋双骏 <841126454@qq.com>
2019-03-16 20:39:38 +0800

    update 单例模式 双重锁检查单例 增加解释不加volatile关键字出现不完整实例的原因

--- design-pattern/src/main/java/study/pattern/singleton/LazySingletonWithDoubleCheck.java
+++ design-pattern/src/main/java/study/pattern/singleton/LazySingletonWithDoubleCheck.java
@@ -8,6 +8,15 @@ package study.pattern.singleton;
  *     (2) 线程安全
  * 缺点:
  *     (1) 如果不加volatile关键词防止指令重排，双重锁检查单例可能会出现不完整实例
+ *         分析：instance = new LazySingletonWithDoubleCheck() 操作并非原子操作，它包含如下三个操作指令：
+ *             1) 分配对象的内存空间 memory = allocate()
+ *             2) 初始化对象 ctorInstance(memory)
+ *             3) 设置instance指向刚分配的内存地址 instance = memory
+ *         经过指令重排序后，执行顺序可能如下：
+ *             1) 分配对象的内存空间 memory = allocate()
+ *             2) 设置instance指向刚分配的内存地址 instance = memory
+ *             3) 初始化对象 ctorInstance(memory)
+ *         若有A线程执行完上述重排序后的第二步，尚未初始化对象，此时B线程来获取单例instance，会发现instance不为空，于是返回该值，但实际该instance尚未构建完成，为不完整实例。
  *     (2) 反序列化，反射与克隆可破坏单例
  */
 public class LazySingletonWithDoubleCheck {
