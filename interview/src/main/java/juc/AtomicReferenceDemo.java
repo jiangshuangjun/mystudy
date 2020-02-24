5b7ce176ba752ccb621f9773ab2d5d0096c8e47b
蒋双骏 <841126454@qq.com>
2020-02-24 23:34:48 +0800

    更新 AtomicReferenceDemo.java

--- interview/src/main/java/juc/AtomicReferenceDemo.java
+++ interview/src/main/java/juc/AtomicReferenceDemo.java
@@ -1,20 +1,31 @@
 package juc;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.ToString;
+
 import java.util.concurrent.atomic.AtomicReference;
 
-class User{
+@Getter
+@ToString
+@AllArgsConstructor
+class User {
     String userName;
     int age;
 }
+
 public class AtomicReferenceDemo {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         AtomicReference<User> atomicReference = new AtomicReference<>();
 
-//        juc.User z3 = new juc.User("z3",22);
-//        juc.User li4 = new juc.User("li4",25);
+        User z3 = new User("z3", 22);
+        User li4 = new User("li4", 25);
 
-//        atomicReference.set(z3);
-//        System.out.println(atomicReference.compareAndSet(z3,li4)+"\t"+atomicReference.get().toString());
+        atomicReference.set(z3);
 
+        // 第一次输出：变更是否成功：true，atomicReference 是: User(userName=li4, age=25)
+        System.out.printf("变更是否成功：%s，atomicReference 是: %s\n", atomicReference.compareAndSet(z3, li4), atomicReference.get().toString());
+        // 第二次输出：变更是否成功：false，atomicReference 是: User(userName=li4, age=25)
+        System.out.printf("变更是否成功：%s，atomicReference 是: %s\n", atomicReference.compareAndSet(z3, li4), atomicReference.get().toString());
     }
 }
