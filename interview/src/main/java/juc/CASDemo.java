2d91b905c043a411af6c8cc677a1767eeaca6842
蒋双骏 <841126454@qq.com>
2020-02-24 23:25:28 +0800

    更新 CASDemo.java

--- interview/src/main/java/juc/CASDemo.java
+++ interview/src/main/java/juc/CASDemo.java
@@ -2,14 +2,14 @@ package juc;
 
 import java.util.concurrent.atomic.AtomicInteger;
 
-/*
-* 1、CAS是什么？ ==>compareAndSet
-*    比较并交换
-* */
+/**
+ * CAS 是什么？ ==> compareAndSet
+ * 比较并交换
+ */
 public class CASDemo {
     public static void main(String[] args){
         AtomicInteger atomicInteger = new AtomicInteger(5);
-        System.out.println(atomicInteger.compareAndSet(5,2019)+"\t current data: "+atomicInteger.get());
-        System.out.println(atomicInteger.compareAndSet(5,1024)+"\t current data: "+atomicInteger.get());
+        System.out.printf("比较并交换是否成功：%s, atomicInteger current value is: %d\n", atomicInteger.compareAndSet(5, 2019), atomicInteger.get());
+        System.out.printf("比较并交换是否成功：%s, atomicInteger current value is: %d\n", atomicInteger.compareAndSet(5, 1024), atomicInteger.get());
     }
 }
